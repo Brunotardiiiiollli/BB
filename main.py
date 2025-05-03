a18b7bd3ccc64be99cb3a5621d67031f126dc0c7
Brunotardiiiiollli <brunotardioli4@gmail.com>
2025-05-03 00:10:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from jarvis_core import iniciar_jarvis
+from interface import InterfaceVisual
+import threading
 
 if __name__ == "__main__":
-    iniciar_jarvis()
+    interface = InterfaceVisual()
+    thread_jarvis = threading.Thread(target=iniciar_jarvis, args=(interface,))
+    thread_jarvis.start()
+    interface.iniciar()
